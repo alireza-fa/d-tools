c587cda4d780bf8232b5d96789dae84add0358d3
alireza-fa <alirezafeyze44@gmail.com>
2024-05-14 21:49:14 +0330

    client.py(get ip_address and device_info)

---
+++ d_tools/client.py
@@ -0,0 +1,31 @@
+import ipaddress
+from typing import Dict
+
+from django.http import HttpRequest
+
+from .constants import IP_ADDRESS, DEVICE_INFO
+
+
+def get_ip_address(request: HttpRequest) -> str:
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    if ip_address:
+        ip_address = ip_address.split(",")[0]
+    else:
+        ip_address = request.META.get("REMOTE_ADDR", '').split(",")[0]
+
+    possibles = (ip_address.lstrip("[").split("]")[0], ip_address.split(":")[0])
+
+    for addr in possibles:
+        try:
+            return str(ipaddress.ip_address(addr))
+        except:
+            pass
+
+    return ip_address
+
+
+def get_client_info(request: HttpRequest) -> Dict:
+    return {
+        DEVICE_INFO: request.META.get('HTTP_USER_AGENT', ''),
+        IP_ADDRESS: get_ip_address(request=request)
+    }
---
+++ d_tools/constants.py
@@ -0,0 +1,2 @@
+IP_ADDRESS = "ip_address"
+DEVICE_INFO = "device_info"
